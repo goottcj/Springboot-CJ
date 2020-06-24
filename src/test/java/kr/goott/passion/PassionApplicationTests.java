4b2a3cb12a24f5ee99ac3de9ab5b2ee4ef49a808
chunjin <chunjin93@naver.com>
2020-06-24 23:16:30 +0900

    20-06-24 11:15 mapper, build gradle setting ...

--- src/test/java/kr/goott/passion/PassionApplicationTests.java
+++ src/test/java/kr/goott/passion/PassionApplicationTests.java
@@ -3,10 +3,11 @@ package kr.goott.passion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class PassionApplicationTests {
-
-	@Test
+	
+	
 	void contextLoads() {
 	}
 
